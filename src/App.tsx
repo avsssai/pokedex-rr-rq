32bcf0808b93aa2ef798dc0d085b88a083afcd90
Siva Addanki <34810623+avsssai@users.noreply.github.com>
2023-11-05 22:27:11 +0530

    added tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+	const [count, setCount] = React.useState(0);
+
+	return (
+		<>
+			<h1 className='text-2xl text-red-500 font-bold'>hello world</h1>
+		</>
+	);
+}
+
+export default App;
